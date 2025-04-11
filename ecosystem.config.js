6d70a0b5a8a434f266087fea56faeac3267d67fd
iamabhi5100 <abhishek.pvs5100@gmail.com>
2025-04-11 11:28:11 +0530

    Wire up PM2 ecosystem & improved deploy workflow

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [{
+      name: 'myapp',
+      script: 'src/index.js',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        DB_HOST: 'localhost',
+        DB_USER: 'myappuser',
+        DB_PASSWORD: 'securepassword',
+        DB_NAME: 'myappdb',
+        DB_PORT: 5432
+      }
+    }]
+  };
+  
